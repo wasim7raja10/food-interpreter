b48be1a8fd6dcbda2efe2cc06f7568a6d275213a
Wasim Raja <wasim7raja10@gmail.com>
2022-08-28 22:11:46 +0530

    project done

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+import { foodEmoji } from "./data/emoji";
+
+const emojis = Object.keys(foodEmoji);
+
+function App() {
+  const [emoji, setEmoji] = useState("empty");
+  const [output, setOutput] = useState("Input is invalid");
+  const onChangeHandler = (event) => {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+    setOutput(foodEmoji[inputEmoji]);
+  };
+  const onClickHandler = (emoji) => {
+    setEmoji(emoji);
+    setOutput(foodEmoji[emoji]);
+  };
+  return (
+    <div className="App">
+      <h1>Food - Interpreter</h1>
+      <input
+        placeholder="Search your food emoji"
+        value={emoji}
+        onChange={onChangeHandler}
+      />
+      <h2>{emoji}</h2>
+      <h1>{output}</h1>
+      <div className="emojis">
+        <h2>you can search these emoji</h2>
+        <div>
+          {emojis.map((emoji, index) => (
+            <span key={index} onClick={() => onClickHandler(emoji)}>
+              {emoji}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/emoji.js
@@ -0,0 +1,13 @@
+export const foodEmoji = {
+  "🍇": "grapes",
+  "🍈": "melon",
+  "🍉": "watermelon",
+  "🍊": "tangerine",
+  "🍌": "banana",
+  "🍍": "pineapple",
+  "🍑": "peach",
+  "🍒": "cherry",
+  "🍓": "strawberry",
+  "🥝": "kiwi",
+  "🍅": "tomato",
+};
